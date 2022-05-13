d643c2ce51c292a70989731335169e9b335c9654
Rafael <rafanavone@gmail.com>
2022-05-13 10:52:27 -0300

    Unfinished project

---
+++ RockPaperScissor.js
@@ -0,0 +1,20 @@
+const firstOption = `Rock`== 0
+const secondOption = `Paper`== 1
+const thirdOption = `Scissor` == 2
+
+
+function ComputerPlay(minimum,maximum){
+  return Math.floor(Math.random() * (maximum-minimum +1) +minimum)  
+}
+
+/*function playerSelection{
+    let output= prompt(`${firstOption},${secondOption},${thirdOption} game.\n Now,pick one and go to the Console`).toLowerCase().toUpperCase()
+    if(output !== firstOption|| output!==secondOption||output!==thirdOption){
+        alert(`Error: Wrong Answer`)
+    }
+    
+}*/
+
+function playRound(playerSelection, ComputerSelection){
+    if 
+}
