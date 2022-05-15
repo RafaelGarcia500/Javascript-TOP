4e077521bf50d6932d462f9e674db3a1128bb3fd
RafaelGarcia500 <91357930+RafaelGarcia500@users.noreply.github.com>
2022-05-15 09:52:30 -0300

    Lacks several features

--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -1,4 +1,4 @@
-const gameOptions = [`Rock`,`Paper`,`Scissors`]
+const gameOptions = [`rock`,`paper`,`scissors`]
 
 function game(){
  playRound()
@@ -7,30 +7,54 @@ function game(){
 function playRound(){
 const playerSelection = playerPlay()
 const computerSelection = computerPlay()
+const winner = checkWinner(playerSelection, computerSelection)
+console.log(winner)
 }
 
 function playerPlay(){
-  let input=prompt(`Rock,Paper,Scissors game. type down below:`)
-  while(input==null){
-    input=prompt(`Rock,Paper,Scissors game. type down below:`)
-  }
-  input = input.toLowerCase()
-  let validate = validateInput(input)
-  while(validate == false){
-     input = prompt(`Type Rock,Paper, or Scissors`)
-     input = input.toLowerCase()
-     validate = validateInput(input)
-  }
+ let input = prompt(`Type Rock,Paper or Scissors`)
+ while(input==null){
+  input = prompt(`Type Rock,Paper or Scissors`)
+ }
+ input = input.toLowerCase()
+ let validate = validateInput(input)
+ while(validate == false){
+   input = prompt(`Type Rock,Paper or Scissors`)
+   while(input==null){
+    input = prompt(`Type Rock,Paper or Scissors`)
+   }
+   input= input.toLowerCase()
+   validate = validateInput(input)
+ }
+ return input
 }
 
 function computerPlay(){
- return gameOptions[Math.floor(Math.random()*gameOptions.lenght)]
-}
+  return gameOptions[Math.floor(Math.random() * gameOptions.length)]
+ }
 
-function validateInput(option){
-    return gameOptions.includes(option)
-}
-        
-    
+ function validateInput(option){
+   if(gameOptions.includes(option)){
+     return true
+   }else{
+     return false
+   }
+ }
 
-game()
+ function checkWinner(player, computer){
+  if(player === computer){
+    return `tie`
+  }else if(
+    (player === `rock` && computer ===`scissors`)||
+    (player === `scissors` && computer ===`paper`)|| 
+    (player === `paper` && computer ===`rock`)
+    ){
+    return ` Player wins.`
+  }else{
+    return `Computer wins.`
+  }
+ }
+ 
+ game()
+
+ //lacks polishment
