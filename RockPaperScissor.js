606198db6da9af0b07a58890d0e314d7e18aef22
RafaelGarcia500 <91357930+RafaelGarcia500@users.noreply.github.com>
2022-05-19 01:02:06 -0300

    Ended project

--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -7,7 +7,9 @@ function game(){
 }
 
 function playRound(){
-playResults()
+  for (let i = 0; i < 5; i++){
+    playResults()
+  }
 }
 
 function playerPlay(){
@@ -15,11 +17,14 @@ function playerPlay(){
   while(input == null ||input == undefined ){
     input = prompt(`Pick between Rock,Paper and Scissors and play against the Computer.`)
   }
+  input=input.toLowerCase()
   if(input == `rock`|| input == `paper` || input == `scissors`){
     console.log(`Player has chosen ${input}`)
   }else{
-    alert(`Enter a valid value`)
+    while(input == null ||input == undefined ){
+    input = prompt(`Pick between Rock,Paper and Scissors and play against the Computer.`)
   }
+}
   return input
 }
 
@@ -31,28 +36,44 @@ function computerPlay(){
 function playResults(){
   const playerSelection = playerPlay()
   const computerSelection = computerPlay()
-
+   
   console.log(`Computer has chosen ${computerSelection}`)
+  
+ 
 
   if(playerSelection == `rock` && computerSelection == `scissors`){
-    console.log(`Rock beats Scissors. Player wins`)
+    console.log(`Rock beats Scissors. Player wins\n
+    
+    `)
   }
   if(playerSelection == `paper` && computerSelection == `rock`){
-    console.log(`Paper beats Rock. Player wins`)
+    console.log(`Paper beats Rock. Player wins\n
+    
+    `)
   }
   if(playerSelection == `scissors` && computerSelection == `paper`){
-    console.log(`Scissors beat Paper. Player wins`)
+    console.log(`Scissors beat Paper. Player wins\n
+    
+    `)
   }
   if(playerSelection == `rock` && computerSelection == `paper`){
-    console.log(`Paper beats Rock. Computer wins`)
+    console.log(`Paper beats Rock. Computer wins\n
+    
+    `)
   }
   if(playerSelection == `scissors` && computerSelection == `rock`){
-    console.log(`Rock beats Scissors. Computer wins`)
+    console.log(`Rock beats Scissors. Computer wins\n`
+    
+    )
   }
   if(playerSelection == `paper` && computerSelection == `scissors`){
-    console.log(`Scissors beat Paper. Computer wins`)
+    console.log(`Scissors beat Paper. Computer wins\n
+    
+    `)
   }else if(playerSelection == computerSelection){
-    console.log(`It is a Tie.`)
+    console.log(`It is a Tie.\n
+    
+    `)
   }
 }
 game()
